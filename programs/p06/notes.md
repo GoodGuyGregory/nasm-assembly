5c0805550974914f9949ea623b64ebe0ac56597e
GoodGuyGregory <greg.witt625@gmail.com>
2019-10-14 09:21:52 -0400

    commits p06 to repo

---
+++ programs/p06/notes.md
@@ -0,0 +1,3 @@
+## External Function Calls to C
+
+[External Function Calls](https://www.csee.umbc.edu/portal/help/nasm/sample_64.shtml)
--- programs/p06/p06.asm
+++ programs/p06/p06.asm
@@ -1,30 +1,41 @@
-extern printf, scanf, strlen
+extern printf
+extern scanf
+extern  strlen
 
 SECTION .data
-fmt: db "The parameter is: %s", 10, 0
+
+fmt: db "Argument to be read is: %s", 10, 0
 fmt2: db "%s", 0
-fmt3: db "Length of parameter: %d", 10, 0
+fmt3: db "Length of Argument: %d", 10, 0
 strlength dd 0
+
+;.bss Section 
 SECTION .bss
 input resw 4
 global main
+
+;Text sectopm
 SECTION .text
+
 main:
-mov rdi, fmt2 ;set type
-mov rsi, input ;grab user input
-mov al, 0
-call scanf ;Call scanf with previous 3 args
-mov rdi, fmt ;set format
-mov rsi, input ;move input into rsi argument for printf
-mov rax, 0 ;no xmm registers used
-call printf ;Call printf with previous 3 registers as args
-mov rdi, input ;move input into rdi
-call strlen ;call strlen with previous arg
-mov [strlength], rax ;move return value from rax into strlength
-mov rdi, fmt3 ;set format
-mov rsi, [strlength] ;move value in strlength to rsi
-mov rax, 0 ;no xmm registers used
-call printf ;call printf with previous 3 registers as args
-mov rax, 60 ; exit syscall
-xor rdi, rdi
-syscall ;end
+        mov rdi, fmt2 ;move the value for the %s to capture the string
+        mov rsi, input ;take in command line arguments
+        mov al, 0 
+        call scanf ;Use Scanf
+        mov rdi, fmt ;Display the argument message
+        mov rsi, input ;move arguments to rsi
+        mov rax, 0
+
+        call printf ;print the results thus far
+        mov rdi, input ;move input into rdi register
+
+        ;string length manipulations
+        call strlen
+        mov [strlength], rax ;makes the value of rax (0) into strlength
+        mov rdi, fmt3 ;display message for the length
+        mov rsi, [strlength] ;dereference for the value 
+        mov rax, 0 
+        call printf ;display the command line results
+        mov rax, 60
+        xor rdi, rdi
+        syscall     ;exit program
