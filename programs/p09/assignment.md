d9047dad020e8223b42466c534bd616e0b70bad1
GoodGuyGregory <greg.witt625@gmail.com>
2019-11-30 20:17:26 -0500

    adds some work to program8 and program9

---
+++ programs/p09/assignment.md
@@ -0,0 +1,6 @@
+**Write a NASM program that will :**
+
+* Declare variables of types `DB, DW, DD, DQ, DT, DO, DY, DZ` and `DT` similar to beginning of [Documentation for types in NASM](https://www.nasm.us/doc/nasmdoc3.html) 
+* Write all of this data out to a file. [Writing Files in NASM](https://www.youtube.com/watch?v=vXsUIX_Ozgc)
+* Use the hexdump to verify that the numbers in the comments are actually present in the file.
+* Capture all, put your notes around the values to show you see the equal values.
---
+++ programs/p09/p09.asm
@@ -0,0 +1,46 @@
+;DEPENDENCIES: 
+; ensure include file is in directory of the project
+;
+; BUILD:
+; nasm -f elf64 p09.asm
+; gcc -m64 -o p09 p09.o
+;
+; EXAMPLE OUTPUT:
+;  
+
+%include "linux64.inc"
+
+SECTION .data
+    ;Create File
+    filename db "myfile.txt", 0
+    ;DECLARE SPECIFIED TYPES:
+    text db "Here's some text."
+
+
+    global main
+SECTION .text
+	main:
+	
+    ; MAIN CODE for the Program
+    ;OPEN FILE:
+    mov rax, SYS_OPEN           ;refer to included file for calls
+    mov rdi, filename           
+    mov rsi, 0_CREAT+0_WRONLY
+    mov rdx, 0644o              ; "o" tells NASM this is an octal value for file permissions
+    syscall
+
+    ;WRITE TO FILE:
+    push rax
+    mov rdi, rax
+    mov rax, SYS_WRITE
+    mov rsi, text
+    mov rdx, 17
+    syscall
+
+    ;CLOSE THE FILE:
+    mov rax, SYS_CLOSE
+    pop rdi
+    syscall
+
+    exit
+
